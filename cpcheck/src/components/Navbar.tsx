94bd4b058b8ed43405c4c6585c2af143aea6b97c
Pwojcik2 <70239167+Pwojcik2@users.noreply.github.com>
2024-06-11 16:55:24 -0700

    Created navbar component

---
+++ cpcheck/src/components/Navbar.tsx
@@ -0,0 +1,58 @@
+import { Link } from "react-router-dom";
+
+function Navbar() {
+    return (
+        <nav className="navbar navbar-expand-lg fixed-top navbar-dark bg-dark">
+  <Link to={'/'} className="navbar-brand" >CapCheck</Link>
+  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
+  </button>
+
+  <div className="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul className="navbar-nav mr-auto">
+      <li className="nav-item active">
+        <Link to={'/'} className="nav-link" >Home <span className="sr-only">(current)</span></Link>
+      </li>
+
+      <li className="nav-item dropdown">
+        <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Teams
+        </a>
+        <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a className="dropdown-item" href="#">Action</a>
+          <a className="dropdown-item" href="#">Another action</a>
+          <div className="dropdown-divider"></div>
+          <a className="dropdown-item" href="#">Something else here</a>
+        </div>
+      </li>
+
+      <li className="nav-item dropdown">
+        <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Players
+        </a>
+        <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a className="dropdown-item" href="#">Action</a>
+          <a className="dropdown-item" href="#">Another action</a>
+          <div className="dropdown-divider"></div>
+          <a className="dropdown-item" href="#">Something else here</a>
+        </div>
+      </li>
+
+      <li className="nav-item active">
+        <Link to={'/'} className="nav-link" >FAQs <span className="sr-only">(current)</span></Link>
+      </li>
+
+      <li className="nav-item active">
+        <Link to={'/'} className="nav-link" >About <span className="sr-only">(current)</span></Link>
+      </li>
+    </ul>
+    <form className="form-inline my-2 my-lg-0">
+      <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
+      <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+    )
+}
+
+export default Navbar
