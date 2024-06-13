40e80d801dd0b3b08f4b65f5453a045a69c1165b
Pwojcik2 <70239167+Pwojcik2@users.noreply.github.com>
2024-06-13 14:54:05 -0700

    added routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './pages/home/App.tsx'
-import Rangers from './pages/teams/rangers/Rangers.tsx'
+import Rangers from './pages/teams/Rangers.tsx'
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
-import Flyers from './pages/teams/flyers/Flyers.tsx'
-import GoldenKnight from './pages/teams/goldenknights/GoldenKnight.tsx';
-import Lightning from './pages/teams/lightning/Lightning.tsx';
+import Flyers from './pages/teams/Flyers.tsx'
+import GoldenKnight from './pages/teams/GoldenKnight.tsx';
+import Lightning from './pages/teams/Lightning.tsx';
+import Wild from './pages/teams/wild.tsx';
+import Islanders from './pages/teams/Islanders.tsx';
+import Capitals from './pages/teams/Capitals.tsx';
+import Canadiens from './pages/teams/Canadiens.tsx';
 
 const router = createBrowserRouter([
   {
@@ -30,6 +34,22 @@ const router = createBrowserRouter([
   {
     path: "teams/lightning",
     element: <Lightning/>
+  },
+  {
+    path: "teams/wild",
+    element: <Wild/>
+  },
+  {
+    path: "teams/islanders",
+    element: <Islanders/>
+  },
+  {
+    path: "teams/capitals",
+    element: <Capitals/>
+  },
+  {
+    path: "teams/canadiens",
+    element: <Canadiens/>
   }
 ]);
 
