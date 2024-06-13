df81c9351ac09037247130fdbd6231004e50e245
Pwojcik2 <70239167+Pwojcik2@users.noreply.github.com>
2024-06-13 14:53:41 -0700

    images added

--- src/assets/Images.tsx
+++ src/assets/Images.tsx
@@ -6,6 +6,14 @@ var rangersDraftImgTraded = <img className="pickTraded" style={{height: '18px', 
 
 // MINNESOTA
 var minnesotaDraft = <img className="im_mid" style={{height: '18px', width: '18px',  marginTop: '4px',}} alt="Logo of the Minnesota Wild" src="https://cdn2.capfriendly.com/images/logos/minnesota_wild.svg"/>
+var minnesotaDraftTraded = <img className="pickTraded" style={{height: '18px', width: '18px',  marginTop: '4px',}} alt="Logo of the Minnesota Wild" src="https://cdn2.capfriendly.com/images/logos/minnesota_wild.svg"/>
+var minnesotaImg = <img style={{ height: '15px', width: '15px', marginRight: '5px', marginTop: '-2px' }} alt="Logo of the wild" title="wild" src="https://cdn2.capfriendly.com/images/logos/minnesota_wild.svg"></img>
+
+//BRUINS
+var bruinsDraft = <img style={{height: '18px', width: '18px',  marginTop: '4px',}} alt="Logo of the Boston Bruins" src="https://cdn2.capfriendly.com/images/logos/boston_bruins.svg"></img>
+
+//BUFFALO DRAFT
+var buffaloDraft = <img style={{height: '18px', width: '18px',  marginTop: '4px',}} alt="Logo of the Buffalo Sabres" src="https://cdn2.capfriendly.com/images/logos/buffalo_sabres_21.svg"></img>
 
 // VANCOUVER
 var vancouverDraft = <img className="im_mid" style={{height: '18px', width: '18px',  marginTop: '4px',}} alt="Logo of the Vancouver Canucks" src="https://cdn2.capfriendly.com/images/logos/vancouver_canucks.svg"/>
@@ -47,7 +55,59 @@ var sharksDraft = <img style={{height: '18px', width: '18px',  marginTop: '4px',
 //COLOMBUS 
 var colombusDraft = <img style={{height: '18px', width: '18px',  marginTop: '4px',}} alt="Logo of the Columbus Blue Jackets" src="https://cdn2.capfriendly.com/images/logos/columbus_blue_jackets.svg"></img>
 
+//LIGHTNING
+var lightningDraftTraded = <img className="pickTraded" style={{height: '18px', width: '18px',  marginTop: '4px',}} alt="Logo of the Tampa Bay Lightning" src="https://cdn2.capfriendly.com/images/logos/tampa_bay_lightning.svg"></img>
+var lightningDraft = <img style={{height: '18px', width: '18px',  marginTop: '4px',}} alt="Logo of the Tampa Bay Lightning" src="https://cdn2.capfriendly.com/images/logos/tampa_bay_lightning.svg"></img>
+var lightningImg = <img style={{ height: '15px', width: '15px', marginRight: '5px', marginTop: '-2px' }} alt="Logo of the lightning" title="lightning" src="https://cdn2.capfriendly.com/images/logos/tampa_bay_lightning.svg"></img>
+
+//OILERS
+var oilersDraft = <img style={{height: '18px', width: '18px',  marginTop: '4px',}} alt="Logo of the Edmonton Oilers" src="https://cdn2.capfriendly.com/images/logos/edmonton_oilers.svg"></img>
+
+//UTAH
+var utahDraft = <img style={{height: '18px', width: '18px',  marginTop: '4px',}} alt="Logo of the Utah" src="https://cdn2.capfriendly.com/images/logos/nhl_shield.svg"></img>
+
+//BLACKHAWKS
+var blackhawksDraft = <img style={{height: '18px', width: '18px',  marginTop: '4px',}} alt="Logo of the Chicago Blackhawks" src="https://cdn2.capfriendly.com/images/logos/chicago_blackhawks.svg"></img>
+
+//TORONOTO
+var torontoDraft = <img style={{height: '18px', width: '18px',  marginTop: '4px',}} alt="Logo of the Toronto Maple Leafs" src="https://cdn2.capfriendly.com/images/logos/toronto_maple_leafs.svg"></img>
+
+//ISLANDERS 
+var islandersDraft = <img style={{height: '18px', width: '18px',  marginTop: '4px',}} alt="Logo of the New York Islanders" src="https://cdn2.capfriendly.com/images/logos/new_york_islanders.svg"></img>
+var islandersDraftTraded = <img className="pickTraded" style={{height: '18px', width: '18px',  marginTop: '4px',}} alt="Logo of the New York Islanders" src="https://cdn2.capfriendly.com/images/logos/new_york_islanders.svg"></img>
+var islandersImg = <img style={{ height: '15px', width: '15px', marginRight: '5px', marginTop: '-2px' }} alt="Logo of the islanders" title="islanders" src="https://cdn2.capfriendly.com/images/logos/new_york_islanders.svg"></img>
+
+//CAPITALS
+var capitalsDraft = <img style={{height: '18px', width: '18px',  marginTop: '4px',}} alt="Logo of the Washington Capitals" src="https://cdn2.capfriendly.com/images/logos/washington_capitals.svg"></img>
+var capitalsDraftTraded = <img className="pickTraded" style={{height: '18px', width: '18px',  marginTop: '4px',}} alt="Logo of the Washington Capitals" src="https://cdn2.capfriendly.com/images/logos/washington_capitals.svg"></img>
+var capitalsImg = <img style={{ height: '15px', width: '15px', marginRight: '5px', marginTop: '-2px' }} alt="Logo of the capitals" title="capitals" src="https://cdn2.capfriendly.com/images/logos/washington_capitals.svg"></img>
+
+//MONTREAL
+var montrealDraft = <img style={{height: '18px', width: '18px',  marginTop: '4px',}} alt="Logo of the Montreal Canadiens" src="https://cdn2.capfriendly.com/images/logos/montreal_canadiens.svg"></img>
+var montrealDraftTraded = <img className="pickTraded" style={{height: '18px', width: '18px',  marginTop: '4px',}} alt="Logo of the Montreal Canadiens" src="https://cdn2.capfriendly.com/images/logos/montreal_canadiens.svg"></img>
+var montrealImg = <img style={{ height: '15px', width: '15px', marginRight: '5px', marginTop: '-2px' }} alt="Logo of the canadiens" title="canadiens" src="https://cdn2.capfriendly.com/images/logos/montreal_canadiens.svg"></img>
+
+//PENGUINS
+var penguinsDraft = <img style={{height: '18px', width: '18px',  marginTop: '4px',}} alt="Logo of the Pittsburgh Penguins" src="https://cdn2.capfriendly.com/images/logos/pittsburgh_penguins.svg"></img>
+
+//DEVILS
+var devilsDraft = <img style={{height: '18px', width: '18px',  marginTop: '4px',}} alt="Logo of the New Jersey Devils" src="https://cdn2.capfriendly.com/images/logos/new_jersey_devils.svg"></img>
+
+//RED WINGS
+var redwWingsDraft = <img style={{height: '18px', width: '18px',  marginTop: '4px',}} alt="Logo of the Detroit Red Wings" src="https://cdn2.capfriendly.com/images/logos/detroit_red_wings.svg"></img>
+
+//CALGARY
+var calgaryDraft = <img style={{height: '18px', width: '18px',  marginTop: '4px',}} alt="Logo of the Calgary Flames" src="https://cdn2.capfriendly.com/images/logos/calgary_flames.svg"></img>
+
+//WINNEPEG
+var winnepegDraft = <img style={{height: '18px', width: '18px',  marginTop: '4px',}} alt="Logo of the Winnipeg Jets" src="https://cdn2.capfriendly.com/images/logos/winnipeg_jets.svg"></img>
+
 export{
+    penguinsDraft,
+    redwWingsDraft,
+    devilsDraft,
+    calgaryDraft,
+    winnepegDraft,
     rangersImg,
     rangersDraftImg,
     rangersDraftImgTraded,
@@ -67,5 +127,21 @@ export{
     sharksDraft,
     knightsDraftTraded,
     knightsImg,
-    colombusDraft
+    colombusDraft,
+    lightningDraft,
+    lightningDraftTraded,
+    lightningImg,
+    oilersDraft,
+    utahDraft,
+    blackhawksDraft,
+    minnesotaDraftTraded,
+    minnesotaImg,
+    buffaloDraft,
+    bruinsDraft,
+    torontoDraft,
+    islandersDraft,
+    islandersDraftTraded,
+    islandersImg,
+    capitalsDraft, capitalsDraftTraded, capitalsImg,
+    montrealDraft, montrealDraftTraded, montrealImg
 }
