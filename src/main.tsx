b6dd5b57da4cb6d9087e106cf69fa3b4c4aca468
Pwojcik2 <70239167+Pwojcik2@users.noreply.github.com>
2024-06-14 15:59:24 -0700

    new routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,60 +1,67 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './pages/home/App.tsx'
-import Rangers from './pages/teams/Rangers.tsx'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import Flyers from './pages/teams/Flyers.tsx'
-import GoldenKnight from './pages/teams/GoldenKnight.tsx';
-import Lightning from './pages/teams/Lightning.tsx';
-import Wild from './pages/teams/wild.tsx';
-import Islanders from './pages/teams/Islanders.tsx';
-import Capitals from './pages/teams/Capitals.tsx';
-import Canadiens from './pages/teams/Canadiens.tsx';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./pages/home/App.tsx";
+import Rangers from "./pages/teams/Rangers.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Flyers from "./pages/teams/Flyers.tsx";
+import GoldenKnight from "./pages/teams/GoldenKnight.tsx";
+import Lightning from "./pages/teams/Lightning.tsx";
+import Wild from "./pages/teams/wild.tsx";
+import Islanders from "./pages/teams/Islanders.tsx";
+import Capitals from "./pages/teams/Capitals.tsx";
+import Canadiens from "./pages/teams/Canadiens.tsx";
+import CapSpace from "./pages/home/CapSpace.tsx";
+import Teams from "./pages/teams/Teams.tsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App/>,
-  },
-  {
-    path: "teams/rangers",
-    element: <Rangers/>
+    element: <App />,
   },
   {
     path: "teams/flyers",
-    element: <Flyers/>
+    element: <Flyers />,
   },
   {
     path: "teams/goldenknights",
-    element: <GoldenKnight/>
+    element: <GoldenKnight />,
   },
   {
     path: "teams/lightning",
-    element: <Lightning/>
+    element: <Lightning />,
   },
   {
     path: "teams/wild",
-    element: <Wild/>
+    element: <Wild />,
   },
   {
     path: "teams/islanders",
-    element: <Islanders/>
+    element: <Islanders />,
   },
   {
     path: "teams/capitals",
-    element: <Capitals/>
+    element: <Capitals />,
   },
   {
     path: "teams/canadiens",
-    element: <Canadiens/>
-  }
+    element: <Canadiens />,
+  },
+  {
+    path: "capspace/hotel/:hotelId",
+    element: <CapSpace />,
+  },
+  {
+    path: "capspace/rangers",
+    element: <CapSpace />,
+  },
+  {
+    path: "teams/rangers",
+    element: <Teams />,
+  },
 ]);
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
