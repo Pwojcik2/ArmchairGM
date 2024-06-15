d939036d14b0aa6c5f33bcdadbeeb082daeba0c2
Pwojcik2 <70239167+Pwojcik2@users.noreply.github.com>
2024-06-14 17:17:06 -0700

    flyers data interface

--- src/types/interfaceTypes.ts
+++ src/types/interfaceTypes.ts
@@ -20,3 +20,20 @@ export interface NewRangersFinancesType {
   todayCapHit?: string;
   id?: string;
 }
+
+export interface FlyersFinancesType {
+  contracts?: string;
+  currentCapSpace?: string;
+  deadlineCapSpace?: string;
+  generalManager?: string;
+  headCoach?: string;
+  injuries?: string;
+  projectedCapHit?: string;
+  projectedCapSpace?: string;
+  projectedLTIR?: string;
+  reserveList?: string;
+  rosterSize?: string;
+  teamName?: string;
+  todayCapHit?: string;
+  id?: string;
+}
