4119d899193f5a99e4dabe3b724cf61089b69cdc
Pwojcik2 <70239167+Pwojcik2@users.noreply.github.com>
2025-01-03 21:22:38 -0700

    Image styling

--- src/components/TeamDraftPicks.tsx
+++ src/components/TeamDraftPicks.tsx
@@ -1,35 +1,45 @@
 import { TeamDraftPicksProps } from "../types/interfaceTypes";
 
 const TeamDraftPicks: React.FC<TeamDraftPicksProps> = ({ data }) => {
+  console.log(data);
+
   return (
-    <>
-      <div>
-        <table className="table">
-          <thead>
-            <tr className="text-center">
-              <th scope="col">DRAFT</th>
-              <th scope="col">ROUND 1</th>
-              <th scope="col">ROUND 2</th>
-              <th scope="col">ROUND 3</th>
-              <th scope="col">ROUND 4</th>
-              <th scope="col">ROUND 5</th>
-              <th scope="col">ROUND 6</th>
-              <th scope="col">ROUND 7</th>
+    <div>
+      <table className="table">
+        <thead>
+          <tr className="text-center">
+            <th scope="col">DRAFT</th>
+            <th scope="col">ROUND 1</th>
+            <th scope="col">ROUND 2</th>
+            <th scope="col">ROUND 3</th>
+            <th scope="col">ROUND 4</th>
+            <th scope="col">ROUND 5</th>
+            <th scope="col">ROUND 6</th>
+            <th scope="col">ROUND 7</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((row, rowIndex) => (
+            <tr className="text-center" key={rowIndex}>
+              <td>{row.year}</td>
+              {row.rounds.map((content, colIndex) => (
+                <td key={colIndex}>
+                  <img
+                    src={content}
+                    alt={`Round ${colIndex + 1}`}
+                    style={{
+                      height: "18px",
+                      width: "18px",
+                      marginTop: "4px",
+                    }}
+                  />
+                </td>
+              ))}
             </tr>
-          </thead>
-          <tbody>
-            {data.map((row, rowIndex) => (
-              <tr className="text-center" key={rowIndex}>
-                <td>{row.year}</td>
-                {row.rounds.map((content, colIndex) => (
-                  <td key={colIndex}>{content}</td>
-                ))}
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-    </>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 };
 
